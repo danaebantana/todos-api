d6da1f4c14c50c89dd1a2c8d109f3175a316c647
danaebantana <danae.bantana@hotmail.com>
2023-01-22 21:42:40 +0200

    added signup and login endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'signup', to: 'users#create'
+
+  post 'auth/login', to: 'authentication#authenticate'
+
   resources :todos do
     resources :items
   end
